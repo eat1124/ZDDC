3f4bda92d0a21f0d5072e23c8577718f56fa7105
miaokela <2972799448@qq.com>
2020-02-24 23:56:29 +0800

    handle_process

--- utils/handle_process.py
+++ utils/handle_process.py
@@ -157,11 +157,16 @@ class Extract(object):
             self.pm = ProcessMonitor.objects.get(app_admin_id=self.app_id, source_id=self.source_id, cycle_id=self.circle_id)
         except ProcessMonitor.DoesNotExist as e:
             logger.info('Extract >> __init__() >> %s' % e)
+        else:
+            if not self.pm:
+                logger.info('Extract >> supplement() >> 该进程不存在，取数进程启动失败。')
+                exit(0)
 
     def supplement(self):
         # 补取
-        if self.pm:
-            start_time = self.pm.last_time
+        start_time = self.pm.last_time
+
+        if start_time:
             end_time = datetime.datetime.now()
 
             # start_time加上一分钟，低于end_time继续循环补取，超过则停止
@@ -171,63 +176,66 @@ class Extract(object):
                 self.get_data(one_minute_plus)
                 if one_minute_plus > end_time:
                     break
+        else:
+            logger.info('Extract >> supplement() >> 首次取数，不进行补取。')
 
-            # 补取过程也消耗时间，再次判断进行补取:先把秒数抹掉再比较(啥意思？)
-            aft_last_time = self.pm.last_time
+        # 补取过程也消耗时间，再次判断进行补取:先把秒数抹掉再比较
+        aft_last_time = self.pm.last_time
 
+        try:
+            aft_last_time = datetime.datetime.strptime('{:%Y-%m-%d %H:%M}'.format(aft_last_time), '%Y-%m-%d %H:%M')
+            now_time = datetime.datetime.strptime('{:%Y-%m-%d %H:%M}'.format(datetime.datetime.now()), '%Y-%m-%d %H:%M')
+        except Exception as e:
+            logger.info('Extract >> supplement() >> %s' % e)
+            exit(0)
+        else:
             try:
-                aft_last_time = datetime.datetime.timestamp('{:%Y-%m-%d %H:%M}'.format(aft_last_time), '%Y-%m-%d %H:%M')
-                now_time = datetime.datetime.timestamp('{:%Y-%m-%d %H:%M}'.format(datetime.datetime.now()), '%Y-%m-%d %H:%M')
+                delta_time = (now_time - aft_last_time).total_seconds()
             except Exception as e:
                 logger.info('Extract >> supplement() >> %s' % e)
                 exit(0)
             else:
-                try:
-                    delta_time = (now_time - aft_last_time).total_seconds()
-                except Exception as e:
-                    logger.info('Extract >> supplement() >> %s' % e)
-                    exit(0)
-                else:
-                    if delta_time > 60:
-                        self.supplement()
+                if delta_time > 60:
+                    self.supplement()
 
     def set_timer(self):
         # 定时器
         while True:
             time_now = datetime.datetime.now()
-            self.get_data(time_now)
+            
+            # 暂时注销
+            # self.get_data(time_now)
             time.sleep(60)
 
     def get_data(self, now_time):
         # 取数
         # 判断当前时间是否符合周期规则
-        if self.pm:
-            circle_rule = self.pm.cycle
-            if circle_rule:
-                schedule_type = circle_rule.schedule_type
-                minute = circle_rule.minute
-                hour = circle_rule.hour
-                day_of_week = circle_rule.day_of_week
-                day_of_month = circle_rule.day_of_month
+        circle_rule = self.pm.cycle
+        if circle_rule:
+            schedule_type = circle_rule.schedule_type
+            minute = circle_rule.minute
+            hour = circle_rule.hour
+            day_of_week = circle_rule.day_of_week
+            day_of_month = circle_rule.day_of_month
 
-                # 当前时间
-                now_minute = now_time.minute
-                now_hour = now_time.hour
-                now_weekday = str(now_time.weekday())
-                now_day = str(now_time.day)
+            # 当前时间
+            now_minute = now_time.minute
+            now_hour = now_time.hour
+            now_weekday = str(now_time.weekday())
+            now_day = str(now_time.day)
 
-                if now_hour == hour and now_minute == minute:
-                    # 每日/每周/每月
-                    if schedule_type == 1:
+            if now_hour == hour and now_minute == minute:
+                # 每日/每周/每月
+                if schedule_type == 1:
+                    self._get_data(now_time)
+                if schedule_type == 2:
+                    # 判断一周第几天
+                    if now_weekday == day_of_week:
                         self._get_data(now_time)
-                    if schedule_type == 2:
-                        # 判断一周第几天
-                        if now_weekday == day_of_week:
-                            self._get_data(now_time)
-                    if schedule_type == 3:
-                        # 判断一月第几天
-                        if now_day == day_of_month:
-                            self._get_data(now_time)
+                if schedule_type == 3:
+                    # 判断一月第几天
+                    if now_day == day_of_month:
+                        self._get_data(now_time)
 
     def _get_data(self, now_time):
         ordered_targets = Target.objects.filter(adminapp_id=self.app_id, source_id=self.source_id, cycle_id=self.circle_id).exclude(state='9').order_by('storage_id', 'storagetag')
@@ -235,14 +243,25 @@ class Extract(object):
         for o_target in ordered_targets:
             storage = o_target.storage
             if storage:
-                if storage.storagetag == '行':
+                # 从DictList中获取
+                storage_type_name = ''
+                try:
+                    dl = DictList.objects.get(id=int(storage.storagetype))
+                except Exception as e:
+                    logger.info('Extract >> _get_data() >> %s' % e)
+                else:
+                    storage_type_name = dl.name
+
+                if storage_type_name == '行':
                     self.get_row_data(o_target, now_time)
-                elif storage.storagetag == '列':
-                    col_ordered_targets = copy_ordered_targets.filter(storage=storage, storagetag=storage.storagetag)
+                    logger.info('获取行数据...')
+                elif storage_type_name == '列':
+                    col_ordered_targets = copy_ordered_targets.filter(storage=storage, storage__storagetype=storage.storagetype)
 
                     self.get_col_data(col_ordered_targets, now_time)
+                    logger.info('获取列数据')
                     # 剔除
-                    copy_ordered_targets = copy_ordered_targets.exclude(storage=storage, storagetag=storage.storagetag)
+                    copy_ordered_targets = copy_ordered_targets.exclude(storage=storage, storagetype=storage.storagetype)
                 else:
                     logger.info('Extract >> _get_data() >> %s' % 'storage_storagetag为空。')
         self.pm.last_time = now_time
@@ -284,7 +303,7 @@ class Extract(object):
             except Exception as e:
                 logger.info('Extract >> get_row_data() >> %s' % e)
             else:
-                source_type_name = source_type.name
+                source_type_name = dl.name
 
             db_query = SeveralDBQuery(source_type_name, source_connection)
             result_list = db_query.fetch_all(source_content)
@@ -364,7 +383,7 @@ class Extract(object):
                 except:
                     logger.info('Extract >> get_col_data() >> %s' % e)
                 else:
-                    source_type_name = source_type.name
+                    source_type_name = dl.name
 
                 db_query = SeveralDBQuery(source_type_name, source_connection)
                 result_list = db_query.fetch_all(source_content)
@@ -521,15 +540,17 @@ class Extract(object):
     def run(self):
         # 补取()
 	    # 启动定时器，每分钟执行一次
-        self.supplement()
+
+        # 暂时注销
+        # self.supplement()
         self.set_timer()
 
 
 def run_process(process_id, processcon, targets):
     # 取数 *****************************************************
-    # SQL Server
-    # connection = pymssql.connect(host='127.0.0.1', user='miaokela', password='Passw0rD', database='mkl')
     pid = os.getpid()
+    logger.info(pid)
+    logger.info(process_id)
     if process_id:
         # for target in targets:
         #     curvalue = getextractdata(target)
@@ -547,62 +568,51 @@ def run_process(process_id, processcon, targets):
         #         extractdata.save()
         process_id = int(process_id)
 
-        # 测试
+        # 实际
         try:
             update_pm = ProcessMonitor.objects.get(id=process_id)
         except ProcessMonitor.DoesNotExist as e:
             logger.info('run_process() >> %s' % e)
         else:
-            update_pm.last_time = datetime.datetime.now()
+            update_pm.create_time = datetime.datetime.now()
             update_pm.p_id = pid
             update_pm.save()
 
-        # # 实际
-        # try:
-        #     update_pm = ProcessMonitor.objects.get(id=process_id)
-        # except ProcessMonitor.DoesNotExist as e:
-        #     logger.info('run_process() >> %s' % e)
-        # else:
-        #     app_id = update_pm.app_admin_id
-        #     source_id = update_pm.source_id
-        #     circle_id = update_pm.cycle_id
-        #     extract = Extract(app_id, source_id, circle_id)
-        #     extract.run()
-
-        #     update_pm.create_time = datetime.datetime.now()
-        #     update_pm.p_id = pid
-        #     update_pm.save()
-        
+            app_id = update_pm.app_admin_id
+            source_id = update_pm.source_id
+            circle_id = update_pm.cycle_id
+            extract = Extract(app_id, source_id, circle_id)
+            extract.run()
     else:
         logger.info('run_process() >> %s' % '传入参数有误。')
 
+# run_process(9, None, None)
 if len(sys.argv) > 1:
-    # # 实际
-    # run_process(sys.argv[1], None, None)
+    run_process(sys.argv[1], None, None)
     logger.info('进程启动。')
-    while True:
-        # if datetime.datetime.now().minute==10:
-        #     connection = pymysql.connect(host='192.168.100.154',
-        #                                  user='root',
-        #                                  password='password',
-        #                                  db='datacenter',
-        #                                  )
-        #     cursor = connection.cursor()
-        #     strsql = "select connection from datacenter_source where id=" + sys.argv[1]
-        #     cursor.execute(strsql)
-        #     data = cursor.fetchall()
-        #     if len(data) > 0:
-        #         processcon = data[0][0]
-        #         strsql = "select id,code,cumulative,sourcetable,sourcefields,sourceconditions from datacenter_target where (state<>'9' or state is null) and operationtype='16' and source_id=" + sys.argv[1]
-        #         cursor.execute(strsql)
-        #         targets = cursor.fetchall()
-        #         if targets>0:
-        #             mytargets = []
-        #             for target in targets:
-        #                 mytargets.append({"id":target[0],"code":target[1],"cumulative":target[2],"sourcetable":target[3],"sourcefields":target[4],"sourceconditions":target[5]})
-        #             run_process(sys.argv[1],processcon,mytargets)
-        #     connection.close()
-        run_process(sys.argv[1], None, None)
-        time.sleep(60)
+    # while True:
+    #     # if datetime.datetime.now().minute==10:
+    #     #     connection = pymysql.connect(host='192.168.100.154',
+    #     #                                  user='root',
+    #     #                                  password='password',
+    #     #                                  db='datacenter',
+    #     #                                  )
+    #     #     cursor = connection.cursor()
+    #     #     strsql = "select connection from datacenter_source where id=" + sys.argv[1]
+    #     #     cursor.execute(strsql)
+    #     #     data = cursor.fetchall()
+    #     #     if len(data) > 0:
+    #     #         processcon = data[0][0]
+    #     #         strsql = "select id,code,cumulative,sourcetable,sourcefields,sourceconditions from datacenter_target where (state<>'9' or state is null) and operationtype='16' and source_id=" + sys.argv[1]
+    #     #         cursor.execute(strsql)
+    #     #         targets = cursor.fetchall()
+    #     #         if targets>0:
+    #     #             mytargets = []
+    #     #             for target in targets:
+    #     #                 mytargets.append({"id":target[0],"code":target[1],"cumulative":target[2],"sourcetable":target[3],"sourcefields":target[4],"sourceconditions":target[5]})
+    #     #             run_process(sys.argv[1],processcon,mytargets)
+    #     #     connection.close()
+    #     run_process(sys.argv[1], None, None)
+    #     time.sleep(60)
 else:
     logger.info('脚本未传参。')
