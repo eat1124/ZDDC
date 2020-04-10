c5d2790fb7101e86c3b5c524128859a0b13875e8
zgs1121 <2451349606@qq.com>
2020-04-10 09:09:45 +0800

    指标管理：数据时间新增最大值最小值、新增复制功能

--- datacenter/views.py
+++ datacenter/views.py
@@ -4694,7 +4694,9 @@ def reporting_formulacalculate(request):
             "D": "当天", "L": "前一天", "MS": "月初", "ME": "月末", "LMS": "上月初", "LME": "上月末",
             "SS": "季初", "SE": "季末", "LSS": "上季初", "LSE": "上季末", "HS": "半年初", "HE": "半年末",
             "LHS": "前个半年初", "LHE": "前个半年末", "YS": "年初", "YE": "年末", "LYS": "去年初",
-            "LYE": "去年末", "MAVG": "月平均值", "SAVG": "季平均值", "HAVG": "半年平均值", "YAVG": "年均值"
+            "LYE": "去年末", "MAVG": "月平均值", "SAVG": "季平均值", "HAVG": "半年平均值", "YAVG": "年均值",
+            "MMAX": "月最大值", "MMIN": "月最小值", "SMAX": "季最大值", "SMIN": "季最小值",
+            "HMAX": "半年最大值", "HMIN": "半年最小值", "YMAX": "年最大值", "YMIN": "年最小值"
         }
 
         calculatedata = getmodels("Calculatedata", str(date.year)).objects.exclude(state="9").filter(
