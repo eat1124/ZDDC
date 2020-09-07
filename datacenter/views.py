b209390e74c3987ee4e59f632e423e445e52d0aa
miaokela <2972799448@qq.com>
2020-09-07 09:20:08 +0800

    debug

--- datacenter/views.py
+++ datacenter/views.py
@@ -7094,8 +7094,8 @@ def login(request):
 
 
 def ad_login(request):
-    username = request.GET.get('username', '')
-    password = request.GET.get('password', '')
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
     user = auth.authenticate(username=username, password=password)
     if user is not None and user.is_active:
         auth.login(request, user)
